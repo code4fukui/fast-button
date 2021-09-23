8e6ce36d74bed738ccb62efed93000a22a05b066
Taisuke Fukuno <fukuno@jig.jp>
2021-09-24 05:49:38 +0900

    add test for ie11

---
+++ fast-button-es5.js
@@ -0,0 +1,38 @@
+class FastButton extends HTMLElement {
+  constructor() {
+    super();
+    if (this.ontouchstart) {
+      this.onaddEventListener("touchstart", (e) => {
+        e.preventDefault();
+        this._fireDown();
+      });
+      this.addEventListener("touchstop", (e) => {
+        e.preventDefault();
+        this._fireUp();
+      });
+    } else {
+      this.addEventListener("mousedown", (e) => {
+        e.preventDefault();
+        this._fireDown();
+      });
+      this.addEventListener("mouseup", (e) => {
+        e.preventDefault();
+        this._fireUp();
+      });
+    }
+  }
+  _fireDown() {
+    if (this.ondown) {
+      this.ondown();
+    }
+  }
+  _fireUp() {
+    if (this.onup) {
+      this.onup();
+    }
+  }
+};
+
+customElements.define("fast-button", FastButton);
+
+//export { FastButton };
